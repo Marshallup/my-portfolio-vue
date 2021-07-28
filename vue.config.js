4643b410db5e71906898f9fd7afa17ffd697ae24
Михаил <televonvea@gmail.com>
2021-07-28 12:08:35 +0300

    first

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://mtes-personal.local',
+  },
+
+  // output built static files to Laravel's public dir.
+  // note the "build" script in package.json needs to be modified as well.
+  outputDir: '../../../public/',
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+
+  // modify the location of the generated HTML file.
+  indexPath: process.env.NODE_ENV === 'production'
+    ? '../resources/views/app.blade.php'
+    : 'index.html',
+};
